04e05cebf78a3d62f609e001919b98f7c760da1f
hello2mao <hello2mao@gmail.com>
2019-02-27 16:53:59 +0800

    add test case

--- test/create-and-delete/create-and-delete.sh
+++ test/create-and-delete/create-and-delete.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-kubectl create -f nginx.yaml
+startTime=`date +%Y%m%d-%H:%M`
+startTime_s=`date +%s`
+
+for i in `seq 1 1`;
+do
+    kubectl create -f nginx.yaml
+    status="Check: "
+    while true
+    do
+        if [ `kubectl get service |grep "nginx-service"|grep -v "pending"|wc -l` -eq 5 ]; then
+            break
+        fi
+        sleep 10
+        status=$status"."
+        echo $status
+    done
+    kubectl delete -f nginx.yaml
+done
+
+endTime=`date +%Y%m%d-%H:%M`
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo "Test Finish: create-and-delete" "$startTime ---> $endTime" "Total:$sumTime minutes"
