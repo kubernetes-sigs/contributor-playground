e87d4be3515850226e31231a88671d4bfa1b7df6
hello2mao <hello2mao@gmail.com>
2019-03-14 10:15:51 +0800

    support udp default

--- pkg/cloud-provider/cce.go
+++ pkg/cloud-provider/cce.go
@@ -41,11 +41,6 @@ const ProviderName = "cce"
 // CceUserAgent is prefix of http header UserAgent
 const CceUserAgent = "cce-k8s:"
 
-// HardSwitch
-const (
-	EnableUDPLBService = false
-)
-
 // Baiducloud defines the main struct
 type Baiducloud struct {
 	CloudConfig
